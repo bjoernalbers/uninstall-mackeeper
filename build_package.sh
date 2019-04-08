dffc01c1f73b2e396967f8f4e7d5f8385d8440bd
Björn Albers <bjoernalbers@gmail.com>
2019-04-08 12:36:47 +0200

    Improve package build script.
    Now we can build packages for development (`./build_package.sh`) and
    release (`./build_package.sh --release`).

--- build_package.sh
+++ build_package.sh
@@ -5,13 +5,44 @@ export PATH='/usr/bin:/bin:/usr/sbin:/sbin'
 
 NAME='Uninstall-MacKeeper'
 IDENTIFIER="com.github.bjoernalbers.${NAME}"
-VERSION="$(git describe --tags)"
+VERSION="$(date '+%Y%m%dT%H%M%SZ')"
 BASE_DIR="$(dirname "$0")"
-BUILD_DIR="${BASE_DIR}/pkgs"
+BUILD_DIR="${BASE_DIR}/tmp"
 SCRIPTS_DIR="${BASE_DIR}/scripts"
-PACKAGE="${BUILD_DIR}/${NAME}-${VERSION}.pkg"
+SUFFIX='pkg'
 
+display_usage() {
+cat <<EOM
+Usage: $0 [OPTIONS]
+
+Options:
+  -h, --help             Display this help and exit
+  -r, --release          Create release package
+EOM
+}
+
+# Process options
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -h|--help)
+      display_usage
+      exit 0
+      ;;
+    -r|--release)
+      VERSION="$(git describe --tags)"
+      BUILD_DIR="${BASE_DIR}/pkgs"
+      ;;
+    *)
+      display_usage >&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# Build package
 mkdir -p "${BUILD_DIR}"
+PACKAGE="${BUILD_DIR}/${NAME}-${VERSION}.${SUFFIX}"
 if [[ ! -e "${PACKAGE}" ]]; then
   pkgbuild \
     --scripts "${SCRIPTS_DIR}" \
